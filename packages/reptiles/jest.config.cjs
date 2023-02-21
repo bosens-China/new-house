122143a5bea1f0956a34ac77f395ddf0fcec4a0b
Yliu <yangboses@gmail.com>
2023-02-21 15:52:52 +0800

    ✨ feat: 调整测试文件位置

--- packages/reptiles/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  resolver: '<rootDir>/mjsResolver.cjs',
-  transform: {
-    '^.+\\.m?(ts|tsx)?$': 'babel-jest',
-    '^.+\\.m?(js|jsx)$': 'babel-jest',
-  },
-};
