a6b38032a8d02aca848c9e24bf68952213ad25be
Yliu <yangboses@gmail.com>
2023-02-23 19:08:11 +0800

    feat: 等待验证测试文件

--- packages/speedLimit/src/index.mts
+++
@@ -1,3 +0,0 @@
-export { alone } from './alone.mjs';
-export { group } from './group.mjs';
-export { getRandomInt } from './utils.mjs';
--- packages/test/src/notice/__test__/template.test.ts
+++ packages/test/src/notice/__test__/template.test.ts
@@ -1,4 +1,4 @@
-import { priceFiltering } from '../../../../notice/src/template/index.mjs';
+import { priceFiltering, getTemplate } from '../../../../notice/src/template/index.mjs';
 
 test(`priceFiltering`, () => {
   // data: Array<CurrentType>,
@@ -45,3 +45,28 @@ test(`priceFiltering`, () => {
   expect(priceFiltering(data, { floorPrice: 2000000, ceilingPrice: 3000000 })[0].building.length).toBe(2);
   expect(priceFiltering(data, { floorPrice: 3000000, ceilingPrice: 4000000 })[0].building.length).toBe(1);
 });
+
+// test('template', () => {
+//   const data = [
+//     {
+//       id: '1',
+//       name: 'name',
+//       link: 'link',
+//       enterprise: '企业',
+//       region: '区域',
+//       startTime: new Date().valueOf(),
+//       startTimeStr: new Date().valueOf().toLocaleString(),
+//       endTime: new Date().valueOf(),
+//       endTimeStr: new Date().valueOf().toLocaleString(),
+//       total: 100,
+//       state: '正在登记',
+//       building: [
+//         {
+//           licence: string;
+//           name: string;
+//           link: string;
+//         }
+//       ];
+//     },
+//   ];
+// });
